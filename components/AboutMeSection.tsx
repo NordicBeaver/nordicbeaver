bbf6482e67dd45b06020d9192ebcafa78fbce56f
NordicBeaver <nordicbeaver@gmail.com>
2021-12-06 11:12:25 +0300

    Update AboutMe text.

--- components/AboutMeSection.tsx
+++ components/AboutMeSection.tsx
@@ -7,16 +7,11 @@ export default function AboutMeSection() {
   return (
     <section className={styles.aboutMeSection}>
       <Container>
-        <SectionHeading>About Me</SectionHeading>
+        <SectionHeading>What I Do</SectionHeading>
         <p>
           I graduated a univesity as a mathematician/programmer about 6 years ago and I&apos;ve worked as a developer
-          since then. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet dui eu erat interdum
-          tincidunt. Donec condimentum leo risus, rhoncus mattis ipsum ultricies sed. Nam tincidunt ligula id orci
-          pretium, ac sagittis neque elementum. Vivamus vitae posuere purus. Sed imperdiet scelerisque varius. Nam
-          aliquet odio laoreet, interdum velit eget, laoreet nibh. Nam eget felis mollis, condimentum turpis at, commodo
-          odio. Aliquam aliquam eros ligula, nec tristique mi rhoncus non. Donec pulvinar ante eu nisi ultricies
-          faucibus. Suspendisse est ipsum, aliquam nec enim vel, accumsan tincidunt lacus. Sed aliquam, velit sit amet
-          ultrices eleifend, augue diam eleifend risus, sed tincidunt nibh massa eu nulla.
+          since then. In my work I got to use many different languages and technologies, but right now I&apos;m most
+          interested in web development. Particularly, in making complex web applications using Typescript/NodeJs/React.
         </p>
       </Container>
     </section>
