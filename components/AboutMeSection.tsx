b0963171fab9cf2357cfa11731b96b4d69b45423
NordicBeaver <nordicbeaver@gmail.com>
2021-12-07 16:50:40 +0300

    Favicon/title/description.

--- components/AboutMeSection.tsx
+++ components/AboutMeSection.tsx
@@ -11,8 +11,8 @@ export default function AboutMeSection() {
         <SectionHeading>What I Do</SectionHeading>
         <p>
           I graduated a univesity as a mathematician/programmer about 6 years ago and I&apos;ve worked as a developer
-          since then. In my work I got to use many different languages and technologies, but right now I&apos;m most
-          interested in web development. Particularly, in making complex web applications using Typescript/NodeJs/React.
+          since then. I got to use many different languages and technologies, but right now I&apos;m most interested in
+          web development. Particularly, in making complex web applications using Typescript/NodeJs/React.
         </p>
       </Container>
     </Section>
